d3485b10416b03d2c8b1a07994b27855252ae941
NiGh10GaLe <ni10gl.com@gmail.com>
2020-04-15 18:43:24 +0200

    some enhancements

--- lib/question.dart
+++ lib/question.dart
@@ -2,8 +2,30 @@ class Question {
   String quesText;
   bool quesAnsr;
 
+  Question(this.quesText, this.quesAnsr);
+}
+/*
+*** You may write the class like this, but the previuos way is better ***
+
+class Question {
+  String quesText;
+  bool quesAnsr;
+
+  Question(String quesText, bool quesAnsr) {
+    this.quesText = quesText;
+    this.quesAnsr = quesAnsr;
+  }
+}
+
+* OR *
+
+class Question {
+  String quesText;
+  bool quesAnsr;
+
   Question(String qTxt, bool qAnsr) {
     quesText = qTxt;
     quesAnsr = qAnsr;
   }
 }
+*/
